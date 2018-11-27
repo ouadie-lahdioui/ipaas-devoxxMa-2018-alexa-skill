f8918b1822b3230848e2e74fccad35cb45c31060
Ouadie Lahdioui <ouadiesoft@gmail.com>
2018-11-27 13:03:07 +0100

    Fixs

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const Alexa = require("alexa-sdk");
 const unirest = require('unirest');
 
 const PHRASES = require("./phrases");
-const WEBHOOK = "https://i-webhooktodbpsql-proj350166.6a63.fuse-ignite.openshiftapps.com/webhook/iasaqOSXL9Q0YNZm6eJqO8L2Tx2ucsTlm4HKJ7DGsRkqLB2cip";
-const id = "123";
+const DEFAULT_WEBHOOK = "https://i-webhooktodbpsql-proj350166.6a63.fuse-ignite.openshiftapps.com/webhook/iasaqOSXL9Q0YNZm6eJqO8L2Tx2ucsTlm4HKJ7DGsRkqLB2cip";
+const WEBHOOK = process.env.WEBHOOK || DEFAULT_WEBHOOK;
 
 const handlers = {
     "LaunchRequest": function () {
@@ -38,28 +38,33 @@ const handlers = {
     "planify": function() {
         console.log(JSON.stringify(`intent= ${JSON.stringify(this.event.request.intent)}`));
 
-        const taskSlot = this.event.request.intent.slots.task;
-
-        if (taskSlot && taskSlot.value) {
-            let task = taskSlot.value.toLowerCase();
-            let body = { id, task};
-
-            console.log(JSON.stringify(`body = ${this.event.request.intent(body)}`));
-
-            unirest
-                .post(WEBHOOK)
-                .type('json')
-                .send(body)
-                .end( response => {
-                    console.log(`response=${JSON.stringify(response)}`);
-                    this.emit(":ask", response.statusCode != 200 ? PHRASES.ON_SUCCESS : PHRASES.TECHNICAL_ERROR);
-                });
-
+        if (this.event.request.dialogState != "COMPLETED"){
+            // return a Dialog.Delegate directive with no updatedIntent property.
+            this.emit(':delegate');
         } else {
-            this.attributes.speechOutput = PHRASES.SLOT_NOT_FOUNDED;
-            this.emit(":ask", this.attributes.speechOutput);
-        }
+            const taskSlot = this.event.request.intent.slots.task;
 
+            if (taskSlot && taskSlot.value) {
+                let task = taskSlot.value.toLowerCase();
+
+                let body = {task};
+
+                console.log(JSON.stringify(`body = ${this.event.request.intent(body)}`));
+
+                unirest
+                    .post(WEBHOOK)
+                    .type('json')
+                    .send(body)
+                    .end( response => {
+                        console.log(`response=${JSON.stringify(response)}`);
+                        this.emit(":ask", response.statusCode != 200 ? PHRASES.ON_SUCCESS : PHRASES.TECHNICAL_ERROR);
+                    });
+
+            } else {
+                this.attributes.speechOutput = PHRASES.SLOT_NOT_FOUNDED;
+                this.emit(":ask", this.attributes.speechOutput);
+            }
+        }
     }
 };
 
